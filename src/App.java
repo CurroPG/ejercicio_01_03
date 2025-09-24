ed73a27102b01b921bb0f8346ac34c6a63e45472
Curro Portillo <211curroportillo@gmail.com>
2025-09-24 18:06:58 +0200

    CAMBIO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%-10s %-10s%n", "computer", "ordeandor");
+        System.out.printf("%-10s %-10s%n", "student", "alumno/a");
+        System.out.printf("%-10s %-10s%n", "cat", "gato");
+        System.out.printf("%-10s %-10s%n", "penguin", "pinguino");
+        System.out.printf("%-10s %-10s%n", "machine", "máquina");
+        System.out.printf("%-10s %-10s%n", "nature", "naturaleza");
+        System.out.printf("%-10s %-10s%n", "light", "luz");
+        System.out.printf("%-10s %-10s%n", "green", "verde");
+        System.out.printf("%-10s %-10s%n", "book", "libro");
+        System.out.printf("%-10s %-10s%n", "pyramid", "pirámide");
+
     }
 }
